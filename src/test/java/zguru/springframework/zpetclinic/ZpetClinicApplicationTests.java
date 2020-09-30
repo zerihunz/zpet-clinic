1ac227c0ca7423c368a8a73b6957867c4f0a83dc
ztegegn <zerihun.tegegn@gmail.com>
2020-09-30 00:45:34 -0400

    Initial Project Add

---
+++ src/test/java/zguru/springframework/zpetclinic/ZpetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package zguru.springframework.zpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
